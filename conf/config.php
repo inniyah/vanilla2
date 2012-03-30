5d211566c75220c870f67573591f3a88a33bccc6
Miriam Ruiz <miriam@debian.org>
2012-03-30 11:43:51 +0200

    Updated configuration

--- conf/config.php
+++ conf/config.php
@@ -17,7 +17,6 @@ $Configuration['EnabledApplications']['Vanilla'] = 'vanilla';
 $Configuration['EnabledLocales']['spanish'] = 'es-ES';
 
 // EnabledPlugins
-$Configuration['EnabledPlugins']['GettingStarted'] = 'GettingStarted';
 $Configuration['EnabledPlugins']['HtmLawed'] = 'HtmLawed';
 $Configuration['EnabledPlugins']['AllViewed'] = TRUE;
 $Configuration['EnabledPlugins']['Flagging'] = TRUE;
@@ -27,22 +26,50 @@ $Configuration['EnabledPlugins']['Tagging'] = TRUE;
 $Configuration['EnabledPlugins']['VanillaStats'] = TRUE;
 $Configuration['EnabledPlugins']['cleditor'] = TRUE;
 $Configuration['EnabledPlugins']['Gravatar'] = TRUE;
+$Configuration['EnabledPlugins']['Voting'] = TRUE;
+$Configuration['EnabledPlugins']['QnA'] = TRUE;
+$Configuration['EnabledPlugins']['LocaleDeveloper'] = TRUE;
+$Configuration['EnabledPlugins']['NillaBlog'] = TRUE;
+$Configuration['EnabledPlugins']['CommentsRSS'] = TRUE;
+$Configuration['EnabledPlugins']['FirstLastNames'] = TRUE;
+$Configuration['EnabledPlugins']['VanillaFancybox'] = TRUE;
+$Configuration['EnabledPlugins']['Quotes'] = TRUE;
+$Configuration['EnabledPlugins']['FileUpload'] = TRUE;
+$Configuration['EnabledPlugins']['UnreadIcon'] = TRUE;
 
 // Garden
 $Configuration['Garden']['Title'] = 'AUla Libre Asturiana';
 $Configuration['Garden']['Cookie']['Salt'] = 'GVCWGY87OR';
 $Configuration['Garden']['Cookie']['Domain'] = '';
-$Configuration['Garden']['Registration']['ConfirmEmail'] = TRUE;
+$Configuration['Garden']['Registration']['ConfirmEmail'] = '1';
+$Configuration['Garden']['Registration']['Method'] = 'Captcha';
+$Configuration['Garden']['Registration']['ConfirmEmailRole'] = '8';
+$Configuration['Garden']['Registration']['CaptchaPrivateKey'] = '6LfSdc8SAAAAADpfjOoW4_Cm0v7EJnW05zbZmujd';
+$Configuration['Garden']['Registration']['CaptchaPublicKey'] = '6LfSdc8SAAAAAH2_frscYrhOqFD6ZZbfq1FKDaqD';
+$Configuration['Garden']['Registration']['InviteExpiration'] = '-1 week';
+$Configuration['Garden']['Registration']['InviteRoles'] = 'a:5:{i:3;s:1:"0";i:4;s:1:"0";i:8;s:1:"0";i:32;s:1:"0";i:16;s:1:"0";}';
 $Configuration['Garden']['Email']['SupportName'] = 'AUla Libre Asturiana';
 $Configuration['Garden']['Version'] = '2.0.18.3';
 $Configuration['Garden']['RewriteUrls'] = FALSE;
 $Configuration['Garden']['CanProcessImages'] = TRUE;
 $Configuration['Garden']['Installed'] = TRUE;
 $Configuration['Garden']['Locale'] = 'es-ES';
+$Configuration['Garden']['InstallationID'] = '8A00-2DACB86F-49E56DAB';
+$Configuration['Garden']['InstallationSecret'] = 'c17d9df9e47624b2f0cb4f77c1bc3b1390d6dcb6';
+$Configuration['Garden']['Html']['SafeStyles'] = FALSE;
 
 // Plugins
 $Configuration['Plugins']['GettingStarted']['Dashboard'] = '1';
 $Configuration['Plugins']['GettingStarted']['Plugins'] = '1';
+$Configuration['Plugins']['GettingStarted']['Categories'] = '1';
+$Configuration['Plugins']['GettingStarted']['Discussion'] = '1';
+$Configuration['Plugins']['GettingStarted']['Registration'] = '1';
+$Configuration['Plugins']['GettingStarted']['Profile'] = '1';
+$Configuration['Plugins']['LocaleDeveloper']['CaptureDefinitions'] = '1';
+$Configuration['Plugins']['LocaleDeveloper']['Key'] = 'aula';
+$Configuration['Plugins']['LocaleDeveloper']['Name'] = 'aula';
+$Configuration['Plugins']['LocaleDeveloper']['Locale'] = 'aula';
+$Configuration['Plugins']['FileUpload']['Enabled'] = TRUE;
 
 // Routes
 $Configuration['Routes']['DefaultController'] = 'discussions';
@@ -51,4 +78,4 @@ $Configuration['Routes']['DefaultController'] = 'discussions';
 $Configuration['Vanilla']['Version'] = '2.0.18.3';
 $Configuration['Vanilla']['AdminCheckboxes']['Use'] = TRUE;
 
-// Last edited by inniyah (85.152.220.76)2012-03-20 09:17:24
+// Last edited by inniyah (212.89.9.53)2012-03-30 05:39:37
