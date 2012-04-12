3d1196124cafc8f73cc01a50f12892ba9a779c6f
Miriam Ruiz <miriam@debian.org>
2012-04-12 17:34:33 +0200

    Added theme: Annotated_CSS version 2.03.1

---
+++ themes/annotated_css/about.php
@@ -0,0 +1,21 @@
+<?php if (!defined('APPLICATION')) exit();
+/*
+Copyright 2008, 2009 Vanilla Forums Inc.
+This file is part of Garden.
+Garden is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+Garden is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with Garden.  If not, see <http://www.gnu.org/licenses/>.
+Contact Vanilla Forums Inc. at support [at] vanillaforums [dot] com
+*/
+
+/**
+ * An associative array of information about this application.
+ */
+$ThemeInfo['Annotated_CSS'] = array(
+   'Name' => 'Annotated_CSS',
+   'Description' => "This theme is designed to help users see what can be easily changed in the Vanilla default style, so they can get 'theming' quickly. The custom.css file contains only the code needed to make the changes, and each of the css rules is annotated to explain what it affects. The 'style' looks horrible, as I've made colours garish so they will stand out. I've also added a Quick Links side panel and a couple of items to the menu.",
+   'Version' => '2.03.1',
+   'Author' => "John Murphy",
+   'AuthorEmail' => 'whu606@gmail.com',
+   'AuthorUrl' => 'http://www.whu606.com'
+);
