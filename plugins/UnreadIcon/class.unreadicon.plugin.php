b2ea21780cbb82de39b2cabc8b80b61043a5396b
Miriam Ruiz <miriam@debian.org>
2012-07-03 12:32:44 +0200

    Upgraded plugin Unread Icon to version 1.2.3

--- plugins/UnreadIcon/class.unreadicon.plugin.php
+++ plugins/UnreadIcon/class.unreadicon.plugin.php
@@ -4,18 +4,45 @@
 $PluginInfo['UnreadIcon'] = array(
     'Name' => 'Unread Icon',
     'Description' => 'Inserts unread icon in front of unread comments and the last comment within indivdual discussions and Recent Icon for messages less than X hours old',
-    'Version' => '1.2.1',
+    'Version' => '1.2.3',
     'RequiredApplications' => array('Vanilla' => '2.0.17.8'),
     'RequiredTheme' => FALSE,
     'RequiredPlugins' => FALSE,
     'HasLocale' => FALSE,
-    'SettingsUrl' => FALSE,
-    'SettingsPermission' => 'Plugins.UnreadIcon.Manage',
+    'SettingsUrl' => '/dashboard/plugin/unreadicon',
+    'RegisterPermissions' => array('Plugins.UnreadIcon.Manage'),
     'Author' => "Peregrine"
 );
 
 class UnreadIcon extends Gdn_Plugin {
 
+ 
+    public function PluginController_UnreadIcon_Create(&$Sender, $Args = array()) {
+        $Sender->Title('Unread Icon');
+        $Sender->AddSideMenu('plugin/authortimeview');
+        $Sender->Form = new Gdn_Form();
+        $Validation = new Gdn_Validation();
+        $ConfigurationModel = new Gdn_ConfigurationModel($Validation);
+        $ConfigurationModel->SetField(array(
+            'Plugins.UnreadIcon.Show_Unread',
+            'Plugins.UnreadIcon.Show_Last',
+            'Plugins.UnreadIcon.Show_Recent',
+            'Plugins.UnreadIcon.Show_Hours'
+        ));
+        $Sender->Form->SetModel($ConfigurationModel);
+
+
+        if ($Sender->Form->AuthenticatedPostBack() === FALSE) {
+            $Sender->Form->SetData($ConfigurationModel->Data);
+        } else {
+            $Data = $Sender->Form->FormValues();
+
+            if ($Sender->Form->Save() !== FALSE)
+                $Sender->StatusMessage = T("Your settings have been saved.");
+        }
+
+        $Sender->Render($this->GetView('ur-settings.php'));
+    }
 
     protected $_Comids;
 
@@ -49,10 +76,6 @@ class UnreadIcon extends Gdn_Plugin {
         return $this->lid;
     }
 
-    public function PluginController_UnreadIcon_Create($Sender, $Args = array()) {
-
-        $Sender->Permission('Plugins.UnreadIcon.Manage');
-    }
 
     protected $unread;
     protected $totcount;
@@ -95,7 +118,7 @@ class UnreadIcon extends Gdn_Plugin {
     protected $cid;
     protected $recentTime;
     protected $insertTime;
-
+    protected $showHours;
     public function DiscussionController_BeforeCommentMeta_Handler($Sender) {
 
         $CommmentIdArray = $this->GetCacheUnread();
@@ -108,28 +131,29 @@ class UnreadIcon extends Gdn_Plugin {
         $cid = $Sender->EventArguments['Comment']->CommentID;
 
         // display last posted icon
-        if ($lid == $cid) {
+        if (($lid == $cid)  && (C('Plugins.UnreadIcon.Show_Last'))) {
             echo sprintf(' <img src="%s" class="LastIcon" title="last posted message in discussion" alt="last recent message in discussion" />', $this->GetWebResource('img/last-icon.png', FALSE, TRUE), $Key);
         }
 
-/* ------------- Uncomment this block to display  icons for recent posts.
-       
+
+        if (C('Plugins.UnreadIcon.Show_Recent')) {
+        $showHours =  (C('Plugins.UnreadIcon.Show_Hours'));
         // display recently posted icon
-        $recentTime = time() - (5 * 3600 );
+        $recentTime = time() - ($showHours * 3600 );
         $insertTime = strtotime($Sender->EventArguments['Comment']->DateInserted);
 
-        if (($lid != $cid) && ($recentTime < $insertTime)) {
+        if   ($recentTime < $insertTime) {
             echo sprintf(' <img src="%s" class="RecentIcon" title="recent comment" alt="recent comment" />', $this->GetWebResource('img/recent-yellow.png', FALSE, TRUE), $Key);
         }
-*/   //---------------    Uncomment this block to display  icons for recent posts.  
+        }
         
-        
-        // display last posted icon
+         if (C('Plugins.UnreadIcon.Show_Unread')) {
+        // display unread icon
         if (($this->userID > 0) && (in_array($cid, $CommmentIdArray))) {
 
             echo sprintf(' <img src="%s" class="UnreadIcon" title="unread comment"  alt="unread comment" />', $this->GetWebResource('img/unread-icon.png', FALSE, TRUE), $Key);
         }
-        
+       } 
    
     }
 
