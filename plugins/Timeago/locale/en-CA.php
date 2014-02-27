153de0646b0294a84100130ef1c12f9edea6db51
Miriam Ruiz <miriam@debian.org>
2014-02-27 18:58:09 +0100

    Added Plugin: Timeago version 1.3 MD5: 69932c743aa8e6e211a68d6047c6b6bd SHA1: 64bba8dbe580ab0040f5e14bf506d9d3e44d7a79 SHA256: 58a8fa2482cfd2940779534c481895b998a85ff647c0192cd274595cf280cf1d SHA512: a1b581e9d2557a8ef1152d0c279d61784e5007934f0059d9f68754a54dcde3e1d04343a4b403751e503347e195190f12d05cea898c918f0c4070ce41976e7d01

---
+++ plugins/Timeago/locale/en-CA.php
@@ -0,0 +1,19 @@
+<?php if (!defined('APPLICATION')) exit();
+
+$Definition['TimeagoPrefixAgo']        = NULL;
+$Definition['TimeagoPrefixFromNow']    = NULL;
+$Definition['TimeagoWordSeparator']    = ' ';
+
+$Definition['Timeago-SuffixAgo']       = 'ago';
+$Definition['Timeago-SuffixFromNow']   = 'from now';
+$Definition['Timeago-Seconds']         = 'less than a minute';
+$Definition['Timeago-Minute']          = 'about a minute';
+$Definition['Timeago-Minutes']         = '%d minutes';
+$Definition['Timeago-Hour']            = 'about an hour';
+$Definition['Timeago-Hours']           = 'about %d hours';
+$Definition['Timeago-Day']             = 'a day';
+$Definition['Timeago-Days']            = '%d days';
+$Definition['Timeago-Month']           = 'about a month';
+$Definition['Timeago-Months']          = '%d months';
+$Definition['Timeago-Year']            = 'about a year';
+$Definition['Timeago-Years']           = '%d years';
